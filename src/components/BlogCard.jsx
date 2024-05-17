78440809d3c03135dc07ec89d15ad7f1f5fce2e9
arafatrahman41 <arafatrahman41@outlook.com>
2024-05-17 22:53:00 +0600

    make dynamic the blog section

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import placeholderImage from '../assets/404.jpg'
+
+const BlogCard = ({blog}) => {
+    const {title, description, cover_image,published_at, id } = blog
+    return (
+        <Link to={`/blog/${id}`} className="max-w-sm mx-auto group transition border-2 hover:scale-105 border-primary border-opacity-30 hover:border-secondary hover:no-underline focus:no-underline dark:bg-gray-50">
+				<img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src={cover_image || placeholderImage} />
+				<div className="p-6 space-y-2">
+					<h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
+					<span className="text-xs dark:text-gray-600">
+                    {new Date(published_at).toLocaleDateString()}
+                    </span>
+					<p>{description}</p>
+				</div>
+			</Link>
+    );
+};
+
+BlogCard.propTypes = {
+   blog: PropTypes.object 
+};
+
+export default BlogCard;
--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Home from './pages/Home.jsx';
-import Blogs from './pages/Blogs.jsx';
-import Bookmarks from './pages/Bookmarks.jsx';
-import MainLayouts from './layouts/MainLayouts.jsx';
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayouts/>,
-    children: [
-      {
-        path: '/',
-        element: <Home />
-      },
-      {
-        path: '/blogs',
-        element: <Blogs/>
-      },
-      {
-        path: '/bookmarks',
-        element: <Bookmarks/>
-      }
-    ]
-  }
-])
+import { router } from './routes/routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <RouterProvider router={router} />
-    {/* <App /> */}
   </>,
 )
